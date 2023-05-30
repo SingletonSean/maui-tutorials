e564af8bc33cf40c900e1f7c651faf510d7eb272
SingletonSean <singletonsean4@gmail.com>
2023-05-29 23:17:24 -0400

    complete mocked mode demo

---
+++ MockedMode/Features/ViewRandomCatFact/Mock/MockRandomCatFactQuery.cs
@@ -0,0 +1,20 @@
+﻿namespace MockedMode.Features.ViewRandomCatFact.Mock
+{
+    public class MockRandomCatFactQuery : IRandomCatFactQuery
+    {
+        private static readonly List<CatFact> CAT_FACTS = new List<CatFact>()
+        {
+            new CatFact() { Content = "Cats are awesome." },
+            new CatFact() { Content = "Cats catch mice." },
+            new CatFact() { Content = "Cats like to sleep." }
+        };
+
+        public Task<CatFact> Execute()
+        {
+            int randomCatFactIndex = new Random().Next(CAT_FACTS.Count);
+            CatFact randomCatFact = CAT_FACTS[randomCatFactIndex];
+
+            return Task.FromResult(randomCatFact);
+        }
+    }
+}
