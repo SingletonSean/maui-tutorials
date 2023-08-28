97bee95d87a863ad3fc9065883f67121e7cdf788
SingletonSean <singletonsean4@gmail.com>
2023-08-27 21:42:44 -0400

    Add extended link component

---
+++ Navigation/Shared/Link.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Navigation.Shared
+{
+    public class Link : Button
+    {
+        public static readonly BindableProperty RouteProperty =
+            BindableProperty.Create(nameof(Route), typeof(string), typeof(Link), "");
+
+        public string Route
+        {
+            get => (string)GetValue(RouteProperty);
+            set => SetValue(RouteProperty, value);
+        }
+
+        public Link()
+        {
+            Clicked += Link_Clicked;
+        }
+
+        private async void Link_Clicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync(Route);
+        }
+    }
+}
