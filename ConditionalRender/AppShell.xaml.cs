95c8299c0b4576cffde3603e94119756058de778
SingletonSean <singletonsean4@gmail.com>
2023-10-16 18:52:06 -0400

    Add initial conditional render demo

---
+++ ConditionalRender/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ConditionalRender
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ConditionalRender/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ConditionalRender
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
