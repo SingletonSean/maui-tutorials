b29831e6ae816b7351861218766382332c3f96cc
SingletonSean <singletonsean4@gmail.com>
2023-11-06 19:31:42 -0500

    Add initial counter demo

---
+++ ReactiveCodeBehind/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReactiveCodeBehind
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ReactiveCodeBehind/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ReactiveCodeBehind
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
