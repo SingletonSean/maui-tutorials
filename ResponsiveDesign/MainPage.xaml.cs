62e1c39d933dfd289a63b9bc4ffc5b5fe36d9d7b
SingletonSean <singletonsean4@gmail.com>
2023-12-11 21:22:29 -0500

    Initial responsive solution

--- ResponsiveDesign/MainPage.xaml.cs
+++ ResponsiveDesign/MainPage.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace ResponsiveDesign
+﻿using System.Globalization;
+
+namespace ResponsiveDesign
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+
+        }
+    }
+
+    public class GreaterThanConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (!double.TryParse(value?.ToString(), out double parsedValue) ||
+                !double.TryParse(parameter?.ToString(), out double parsedParameter))
+            {
+                return false;
+            }
+
+            return parsedValue > parsedParameter;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 
--- ResponsiveDesign/MauiProgram.cs
+++ ResponsiveDesign/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace ResponsiveDesign
 {
@@ -9,6 +10,7 @@ namespace ResponsiveDesign
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
