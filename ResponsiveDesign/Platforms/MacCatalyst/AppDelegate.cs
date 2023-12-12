d932e964a4366c95df6501b7108e94e692cf2220
SingletonSean <singletonsean4@gmail.com>
2023-12-11 20:35:35 -0500

    Add responsive design demo

---
+++ ResponsiveDesign/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ResponsiveDesign
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
