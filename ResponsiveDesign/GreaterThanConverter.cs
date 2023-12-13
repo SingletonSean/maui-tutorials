ddf54b2ca5bd479453add96486727fbcaadecf46
SingletonSean <singletonsean4@gmail.com>
2023-12-12 23:18:58 -0500

    Implement responsiveness

---
+++ ResponsiveDesign/GreaterThanConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiveDesign
+{
+    public class GreaterThanConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            if (!double.TryParse(value?.ToString(), out double parsedValue) ||
+                !double.TryParse(parameter?.ToString(), out double parsedParameter))
+            {
+                return false;
+            }
+
+            return parsedValue > parsedParameter;
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
