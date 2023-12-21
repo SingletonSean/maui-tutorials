3da62619e998874b4a08bb56da64dcfc71cefedd
SingletonSean <singletonsean4@gmail.com>
2023-12-20 20:00:35 -0500

    Initialize continuous deployment project

---
+++ ContinuousDeployment/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ContinuousDeployment
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
