863c76f72fa1e4f7e7c298526166abc163d4d441
SingletonSean <singletonsean4@gmail.com>
2024-04-08 08:49:21 -0400

    Move outline into default style

--- ComponentsOverStyles/AppButton.cs
+++ ComponentsOverStyles/AppButton.cs
@@ -8,17 +8,32 @@
     public class AppButton : Button
     {
         public static readonly BindableProperty VariantProperty =
-            BindableProperty.Create(nameof(Variant), typeof(VariantType), typeof(AppButton), null, 
-                propertyChanged: OnStylePropertyChanged);
+            BindableProperty.Create(nameof(Variant), typeof(VariantType), typeof(AppButton), null,
+                propertyChanged: OnVariantPropertyChanged);
         public VariantType Variant
         {
             get => (VariantType)GetValue(VariantProperty);
             set => SetValue(VariantProperty, value);
         }
 
+        public static readonly BindablePropertyKey VariantBackgroundColorProperty =
+            BindableProperty.CreateReadOnly(nameof(VariantBackgroundColor), typeof(Color), typeof(AppButton), null);
+        public Color VariantBackgroundColor
+        {
+            get => (Color)GetValue(VariantBackgroundColorProperty.BindableProperty);
+            private set => SetValue(VariantBackgroundColorProperty, value);
+        }
+
+        public static readonly BindablePropertyKey VariantForegroundColorProperty =
+            BindableProperty.CreateReadOnly(nameof(VariantForegroundColor), typeof(Color), typeof(AppButton), null);
+        public Color VariantForegroundColor
+        {
+            get => (Color)GetValue(VariantForegroundColorProperty.BindableProperty);
+            private set => SetValue(VariantForegroundColorProperty, value);
+        }
+
         public static readonly BindableProperty OutlineProperty =
-            BindableProperty.Create(nameof(Outline), typeof(bool), typeof(AppButton), false,
-                propertyChanged: OnStylePropertyChanged);
+            BindableProperty.Create(nameof(Outline), typeof(bool), typeof(AppButton), false);
         public bool Outline
         {
             get => (bool)GetValue(OutlineProperty);
@@ -27,50 +42,32 @@
 
         public AppButton()
         {
-            UpdateStyle();
+            UpdateVariantColors();
         }
 
-        private static void OnStylePropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        private static void OnVariantPropertyChanged(BindableObject bindable, object oldValue, object newValue)
         {
             if (bindable is AppButton button)
             {
-                button.UpdateStyle();
+                button.UpdateVariantColors();
             }
         }
 
-        private void UpdateStyle()
-        {
-            VariantColor? variantColor = GetVariantColor();
-
-            if (Outline)
-            {
-                BackgroundColor = Color.FromArgb("#ffffff");
-                TextColor = Color.FromArgb("#000000");
-                BorderWidth = 1;
-                BorderColor = variantColor?.Background;
-            }
-            else
-            {
-                BackgroundColor = variantColor?.Background;
-                TextColor = variantColor?.Foreground;
-                BorderWidth = 0;
-                BorderColor = null;
-            }
-        }
-
-        private VariantColor? GetVariantColor()
+        private void UpdateVariantColors()
         {
             switch (Variant)
             {
                 case VariantType.Primary:
-                    return new VariantColor(Color.FromArgb("#475BA1"), Color.FromArgb("#ffffff"));
+                    VariantBackgroundColor = Color.FromArgb("#475BA1");
+                    VariantForegroundColor = Color.FromArgb("#ffffff");
+                    break;
                 case VariantType.Secondary:
-                    return new VariantColor(Color.FromArgb("#FFD97A"), Color.FromArgb("#000000"));
+                    VariantBackgroundColor = Color.FromArgb("#FFD97A");
+                    VariantForegroundColor = Color.FromArgb("#000000");
+                    break;
                 default:
-                    return null;
+                    break;
             }
         }
-
-        private record VariantColor(Color Background, Color Foreground);
     }
 }
