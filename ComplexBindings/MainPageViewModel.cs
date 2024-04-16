b5e8f1ee5cbcb981d8f8ac5a9b9409b925e4afa5
SingletonSean <singletonsean4@gmail.com>
2024-04-15 20:56:13 -0400

    Add complex bindings solution

--- ComplexBindings/MainPageViewModel.cs
+++ ComplexBindings/MainPageViewModel.cs
@@ -5,6 +5,13 @@ namespace ComplexBindings
     public partial class MainPageViewModel : ObservableObject
     {
         [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsGood))]
+        [NotifyPropertyChangedFor(nameof(IsMedium))]
+        [NotifyPropertyChangedFor(nameof(IsBad))]
         private double _yourScore = 100;
+
+        public bool IsGood => YourScore > 75;
+        public bool IsMedium => !IsGood && !IsBad;
+        public bool IsBad => YourScore < 25;
     }
 }
